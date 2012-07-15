f2f1f888dfe7345951ba1f1bc86954c25516f7e6
Michael Snoyman <michael@snoyman.com>
2012-07-15 09:17:48 +0300

    Text.Markdown.Block

---
+++ Text/Markdown/Block.hs
@@ -0,0 +1,113 @@
+{-# LANGUAGE OverloadedStrings #-}
+module Text.Markdown.Block
+    ( Block (..)
+    , ListType (..)
+    , toBlocks
+    ) where
+
+import Data.Conduit
+import qualified Data.Conduit.Text as CT
+import qualified Data.Conduit.List as CL
+import Data.Text (Text)
+import qualified Data.Text as T
+import Data.Functor.Identity (runIdentity)
+import Data.Char (isDigit)
+
+data ListType = Ordered | Unordered
+  deriving (Show, Eq)
+
+data Block inline
+    = BlockPara inline
+    | BlockList ListType (Either inline [Block inline])
+    | BlockCode (Maybe Text) Text
+    | BlockQuote [Block inline]
+  deriving (Show, Eq)
+
+toBlocks :: Monad m => GInfConduit Text m (Block Text)
+toBlocks = mapOutput noCR CT.lines >+> toBlocksLines
+
+toBlocksLines :: Monad m => GInfConduit Text m (Block Text)
+toBlocksLines = injectLeftovers $ awaitForever start
+
+noCR :: Text -> Text
+noCR t
+    | T.null t = t
+    | T.last t == '\r' = T.init t
+    | otherwise = t
+
+start :: Monad m => Text -> GLConduit Text m (Block Text)
+start t
+    | T.null $ T.strip t = return ()
+    | Just lang <- T.stripPrefix "~~~" t = do
+        (finished, ls) <- takeTill (== "~~~") >+> withUpstream CL.consume
+        if finished
+            then yield $ BlockCode (if T.null lang then Nothing else Just lang) $ T.intercalate "\n" ls
+            else mapM_ leftover (reverse $ T.cons ' ' t : ls)
+    | Just t' <- T.stripPrefix "> " t = do
+        ls <- takeQuotes >+> CL.consume
+        let blocks = runIdentity $ mapM_ yield (t' : ls) $$ toBlocksLines =$ CL.consume
+        yield $ BlockQuote blocks
+    | Just t' <- T.stripPrefix "    " t = do
+        ls <- getIndented 4 >+> CL.consume
+        yield $ BlockCode Nothing $ T.intercalate "\n" $ t' : ls
+    | Just (ltype, t') <- listStart t = do
+        let (spaces, t'') = T.span (== ' ') t'
+        if T.length spaces >= 2
+            then do
+                let leader = T.length t - T.length t''
+                ls <- getIndented leader >+> CL.consume
+                let blocks = runIdentity $ mapM_ yield (t'' : ls) $$ toBlocksLines =$ CL.consume
+                yield $ BlockList ltype $ Right blocks
+            else yield $ BlockList ltype $ Left t''
+
+    | otherwise = do
+        ls <- takeTill (T.null . T.strip) >+> CL.consume
+        yield $ BlockPara $ T.unwords $ t : ls
+
+takeTill f =
+    loop
+  where
+    loop = await >>= maybe (return False) (\x -> if f x then return True else yield x >> loop)
+
+listStart t
+    | Just t' <- T.stripPrefix "* " t = Just (Unordered, t')
+    | Just t' <- stripNumber t, Just t'' <- stripSeparator t' = Just (Ordered, t'')
+    | otherwise = Nothing
+
+stripNumber x
+    | T.null y = Nothing
+    | otherwise = Just z
+  where
+    (y, z) = T.span isDigit x
+
+stripSeparator x =
+    case T.uncons x of
+        Nothing -> Nothing
+        Just ('.', y) -> Just y
+        Just (')', y) -> Just y
+        _ -> Nothing
+
+getIndented :: Monad m => Int -> GLConduit Text m Text
+getIndented leader =
+    go []
+  where
+    go blanks = await >>= maybe (mapM_ leftover blanks) (go' blanks)
+
+    go' blanks t
+        | T.null $ T.strip t = go (T.drop leader t : blanks)
+        | T.length x == leader && T.null (T.strip x) = do
+            mapM_ yield $ reverse blanks
+            yield y
+            go []
+        | otherwise = mapM_ leftover blanks >> leftover t
+      where
+        (x, y) = T.splitAt leader t
+
+takeQuotes :: Monad m => GLConduit Text m Text
+takeQuotes =
+    await >>= maybe (return ()) go
+  where
+    go ">" = yield "" >> takeQuotes
+    go t
+        | Just t' <- T.stripPrefix "> " t = yield t' >> takeQuotes
+        | otherwise = leftover t
---
+++ test/block.hs
@@ -0,0 +1,77 @@
+{-# LANGUAGE OverloadedStrings #-}
+import Test.Hspec.Monadic
+import Test.Hspec.HUnit ()
+import Test.HUnit hiding (Test)
+import Text.Markdown.Inline
+import Data.Text (Text)
+import Text.Blaze.Renderer.Text (renderHtml)
+import Control.Monad (forM_)
+import Data.Attoparsec.Text
+import Data.Conduit
+import qualified Data.Conduit.List as CL
+import Text.Markdown.Block
+import Data.Functor.Identity (runIdentity)
+
+check :: Text -> [Block Text] -> Assertion
+check md blocks = runIdentity (yield md $$ toBlocks =$ CL.consume) @?= blocks
+
+main :: IO ()
+main = hspec $ do
+    describe "tilde code" $ do
+        it "simple" $ check
+            "~~~haskell\nfoo\n\nbar\n~~~"
+            [BlockCode (Just "haskell") "foo\n\nbar"]
+        it "no lang" $ check
+            "~~~\nfoo\n\nbar\n~~~"
+            [BlockCode Nothing "foo\n\nbar"]
+        it "no close" $ check
+            "~~~\nfoo\n\nbar\n"
+            [BlockPara " ~~~ foo", BlockPara "bar"]
+    describe "list" $ do
+        it "simple" $ check
+            "* foo\n*    bar"
+            [ BlockList Unordered (Left "foo")
+            , BlockList Unordered (Right [BlockPara "bar"])
+            ]
+        it "nested" $ check
+            "* foo\n*    1. bar\n     2. baz"
+            [ BlockList Unordered (Left "foo")
+            , BlockList Unordered (Right
+                [ BlockList Ordered $ Left "bar"
+                , BlockList Ordered $ Left "baz"
+                ])
+            ]
+        it "with blank" $ check
+            "*   foo\n\n    bar\n* baz"
+            [ BlockList Unordered $ Right
+                [ BlockPara "foo"
+                , BlockPara "bar"
+                ]
+            , BlockList Unordered $ Left "baz"
+            ]
+    describe "blockquote" $ do
+        it "simple" $ check
+            "> foo\n>\n> * bar"
+            [ BlockQuote
+                [ BlockPara "foo"
+                , BlockList Unordered $ Left "bar"
+                ]
+            ]
+        it "blank" $ check
+            "> foo\n\n> * bar"
+            [ BlockQuote [BlockPara "foo"]
+            , BlockQuote [BlockList Unordered $ Left "bar"]
+            ]
+    describe "indented code" $ do
+        it "simple" $ check
+            "    foo\n    bar\n"
+            [ BlockCode Nothing "foo\nbar"
+            ]
+        it "blank" $ check
+            "    foo\n\n    bar\n"
+            [ BlockCode Nothing "foo\n\nbar"
+            ]
+        it "extra space" $ check
+            "    foo\n\n     bar\n"
+            [ BlockCode Nothing "foo\n\n bar"
+            ]
